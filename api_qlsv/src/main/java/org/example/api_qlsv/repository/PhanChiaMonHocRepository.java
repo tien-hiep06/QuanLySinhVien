2613fcb90fabefe3f74b8ed0606aa115adb3fae8
DESKTOP-00EU68T\hinoe <tienhiep0612@gmail.com>
2025-03-14 03:00:19 +0700

    update code api controller

--- api_qlsv/src/main/java/org/example/api_qlsv/repository/PhanChiaMonHocRepository.java
+++ api_qlsv/src/main/java/org/example/api_qlsv/repository/PhanChiaMonHocRepository.java
@@ -1,9 +1,8 @@
 package org.example.api_qlsv.repository;
 
 import org.example.api_qlsv.model.PhanChiaMonHoc;
-import org.example.api_qlsv.model.PhanChiaMonHocId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PhanChiaMonHocRepository extends JpaRepository<PhanChiaMonHoc, PhanChiaMonHocId> {
+public interface PhanChiaMonHocRepository extends JpaRepository<PhanChiaMonHoc, Long> {
 
 }
